fbdf9ba396b2d13d817dfddb1aec040db3c8f2c1
armando-ai <aramirezcanas@student.neumont.edu>
2023-07-23 02:32:59 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `${process.env.UI}`,
+    origin: `https://green-guys-landscaping.vercel.app/`,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
